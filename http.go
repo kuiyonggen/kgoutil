4142df296cdd5b0397d2fc1966bff11a81ad1d52
kyg <dev@kuiyonggen.com>
2025-05-20 11:59:33 +0800

    check if nil

--- http.go
+++ http.go
@@ -1,36 +1,50 @@
 package kgoutil
 
 import (
+    "io"
     "net/url"
     "net/http"
     "encoding/json"
 )
 
 
-func Get(baseUrl string, headers map[string]string, params map[string]string, entity interface{}) error {
+
+func GetBody(baseUrl string, headers map[string]string, params map[string]string) (error, io.ReadCloser) {
     client := &http.Client{}
     endpoint, err := url.Parse(baseUrl)
     if err != nil {
-        return err
+        return err, nil
     } else {
-        queryParams := url.Values{}
-        for k, v := range params {
-            queryParams.Add(k, v)
-        }
+        if headers != nil {
+            queryParams := url.Values{}
+            for k, v := range params {
+                queryParams.Add(k, v)
+            }
 
-        endpoint.RawQuery = queryParams.Encode()
+            endpoint.RawQuery = queryParams.Encode()
+        }
 
         req, err := http.NewRequest(http.MethodGet, endpoint.String(), nil)
         if err != nil {
-            return err
+            return err, nil
         } else {
-            for k, v := range headers {
-                req.Header.Add(k, v)
+            if headers != nil {
+                for k, v := range headers {
+                    req.Header.Add(k, v)
+                }
             }
             resp, _ := client.Do(req)
-            defer resp.Body.Close()
-            return json.NewDecoder(resp.Body).Decode(&entity)
-        }
+            return nil, resp.Body
+       }
     }
+}
+
+func Get(baseUrl string, headers map[string]string, params map[string]string, entity interface{}) error {
+    err, body := GetBody(baseUrl, headers, params)
+    if err != nil {
+        return err
+    }
+    defer body.Close()
+    return json.NewDecoder(body).Decode(&entity)
 }
 
